f226c81e1864ad27b6978d77551d676c3c75c596
Mike Cooper <mikescooper@icloud.com>
2019-09-08 19:22:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ end
 
 def add_item(title, price, quantity=1)
   @total += (price * quantity)
+  items << title 
 end 
 
 def apply_discount
@@ -25,7 +26,7 @@ def apply_discount
 end 
 
 def items
-  
+  items = []
 end 
 
 
