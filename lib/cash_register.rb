648b8558511a87aabe213d10af1149f28c39966c
Mike Cooper <mikescooper@icloud.com>
2019-09-08 19:17:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,12 +14,12 @@ def add_item(title, price, quantity=1)
 end 
 
 def apply_discount
-  percent = @discount / 100.to_f
-  total_with_discount =  @total - (@total * percent) 
 
   if !@discount  
     "There is no discount to apply." 
   else
+    percent = @discount / 100.to_f
+    total_with_discount =  @total - (@total * percent)
     @total = total_with_discount.to_i
     "After the discount, the total comes to $#{@total}."
   end 
