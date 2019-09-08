3c2665b4a2e6557a04010f9d510c23b229954cae
Mike Cooper <mikescooper@icloud.com>
2019-09-08 19:16:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,12 +17,13 @@ def apply_discount
   percent = @discount / 100.to_f
   total_with_discount =  @total - (@total * percent) 
 
-  if @discount == nil  
+  if !@discount  
     "There is no discount to apply." 
   else
     @total = total_with_discount.to_i
     "After the discount, the total comes to $#{@total}."
   end 
+  
 end 
 
 
