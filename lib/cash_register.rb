51a2a4ccaaba255b0ec79ba1709898474511f34b
Mike Cooper <mikescooper@icloud.com>
2019-09-08 19:04:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+require 'pry'
 
+class CashRegister
+  
+attr_accessor :total, :discount 
+
+def initialize(discount = nil)
+  @total = 0
+  @discount = discount
+end 
+
+def add_item(title, price, quantity=1)
+  @total += (price * quantity)
+end 
+
+def apply_discount
+  percent = @discount / 100
+  total_with_discount =  @total * pecent 
+  binding.pry 
+
+  @discount == nil ? "There is no discount to apply." : self.total = total_with_discount
+
+end 
+
+
+
+
+
+
+end
